9e5e58fda4e1e071c22a53cc857cf64ee1de0e69
Kevin Nguyen <72723199+kevinhoangnn@users.noreply.github.com>
2023-09-16 03:13:05 -0700

    comments on code

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,8 +1,17 @@
+/*
+    This file is used to connect to the MongoDB database.
+    It uses the mongoose library to connect to the database.
+    It uses the dotenv library to access the environment variables.
+*/
+
+// Importing modules
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// Creating the connectDB function
 const connectDB = async () => {
     try {
+        // Connect to the database with the MONGO_URI environment variable
         await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB connected!`);
     } catch (error) {
@@ -11,4 +20,5 @@ const connectDB = async () => {
     }
 };
 
+// Exporting the connectDB function
 module.exports = connectDB;
--- model/User.js
+++ model/User.js
@@ -1,7 +1,16 @@
+/*
+    This file contains the User model and functions to create a new user and check if an email is valid.
+    The User model is used to create a new user in the database.
+    The createUser function is used to create a new user in the database.
+    The isValidEmail function is used to check if an email is valid.
+*/
+
+// Importing modules
 const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 const bcrypt = require('bcrypt');
 
+// Creating the User Schema
 const User = new mongoose.Schema({
     email: {
         type: String,
@@ -20,12 +29,16 @@ const User = new mongoose.Schema({
     }
 });
 
+// Adding passport-local-mongoose to User Schema
 User.plugin(passportLocalMongoose);
 
+// Creating the User model
 const UserModel = mongoose.model('User', User);
 
+// Creating the createUser function
 async function createUser(input_email, input_username, input_password) {
     try {
+        // Check if email or username is already taken
         const emailTaken = await UserModel.findOne({ email: input_email });
         if (emailTaken) {
             throw new Error("Email already in use");
@@ -36,9 +49,11 @@ async function createUser(input_email, input_username, input_password) {
             throw new Error("Username already in use");
         }
 
+        // Hash the password
         const saltRounds = 10;
         const input_password_hash = bcrypt.hashSync(input_password, saltRounds);
     
+        // Create the new user with the hashed password
         const newUser = await UserModel.create({
             email: input_email,
             username: input_username,
@@ -52,12 +67,15 @@ async function createUser(input_email, input_username, input_password) {
     }
 }
 
+// Creating the isValidEmail function
 function isValidEmail(input_email) {
+    // Regex for email validation
     const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
 
     return emailRegex.test(input_email);
 }
 
+// Exporting the User model and functions
 module.exports = {
     UserModel,
     createUser,
