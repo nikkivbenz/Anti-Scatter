8cdb8ab87d135b81c654d05a707554fbe14aca03
Kyzoomi <103146202+Kyzoomi@users.noreply.github.com>
2023-09-16 01:01:31 -0700

    Fixed code

--- page-limit/src/App.js
+++ page-limit/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const BlockedWebsites = () => {
+  const [websites, setWebsites] = useState([]);
+  const [websiteUrl, setWebsiteUrl] = useState('');
+  const [usageTime, setUsageTime] = useState(0);
+
+  const addWebsite = () => {
+    if (websiteUrl && usageTime > 0) {
+      const newWebsite = { url: websiteUrl, limit: usageTime };
+      setWebsites([...websites, newWebsite]);
+      setWebsiteUrl('');
+      setUsageTime(0);
+    }
+  };
+
+  const removeWebsite = (url) => {
+    const updatedWebsites = websites.filter((website) => website.url !== url);
+    setWebsites(updatedWebsites);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Blocked Websites</h2>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter website URL"
+          value={websiteUrl}
+          onChange={(e) => setWebsiteUrl(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Usage time limit (minutes)"
+          value={usageTime}
+          onChange={(e) => setUsageTime(parseInt(e.target.value))}
+        />
+        <button onClick={addWebsite}>Add</button>
+      </div>
+      <ul>
+        {websites.map((website, index) => (
+          <li key={index}>
+            {website.url} - {website.limit} minute(s)
+            <button onClick={() => removeWebsite(website.url)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default BlockedWebsites;
