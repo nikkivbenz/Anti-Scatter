e9ab265ffafafc9b0eea61d2bbd0d4f475b29934
Kevin Nguyen <72723199+kevinhoangnn@users.noreply.github.com>
2023-10-05 02:17:33 -0700

    Trying to fix frontend to backend conenction

--- client/anti-scatter/src/index.js
+++ client/anti-scatter/src/index.js
@@ -4,15 +4,14 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
