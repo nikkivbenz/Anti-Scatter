fe3ddfe3aed1b572ef5d06f5596bc39f90541720
Kyzoomi <103146202+Kyzoomi@users.noreply.github.com>
2023-10-27 01:23:58 -0700

    Update App.js

--- daily_quote/src/App.js
+++ daily_quote/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Quote from './Quote';
 
-function App() {
+const App = () => {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  useEffect(() => {
+    const category = 'happiness'; // Change this to your desired category
+    const apiKey = 'cMaMA9kZvA5zQKEDBpIFvw==FFH42gO9nLTanwq8'; // Replace with your actual API key
+
+    fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+      method: 'GET',
+      headers: {
+        'X-Api-Key': apiKey,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.length > 0) {
+          setQuote(data[0].quote);
+          setAuthor(data[0].author);
+        }
+      })
+      .catch((error) => {
+        console.error('Error: ', error);
+      });
+  }, []); // The empty dependency array ensures this effect runs once on component mount
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quote of the Day</h1>
+      <Quote quote={quote} author={author} />
     </div>
   );
-}
+};
 
 export default App;
