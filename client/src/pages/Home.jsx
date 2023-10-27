aeeba4adf8dbb7d47de6d6e3775b5e6e59991791
Kyzoomi <103146202+Kyzoomi@users.noreply.github.com>
2023-10-27 03:19:56 -0700

    final

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
-
 /*
     This is the home page of the application. It is a protected route, which means that the user can only access it if he/she is logged in.
 */
@@ -12,6 +11,9 @@ const Home = () => {
     const navigate = useNavigate();
     const [cookies, removeCookie] = useCookies([]);
     const [username, setUsername] = useState("");
+    const [quotes, setQuotes] = useState([]);
+    const [quote, setQuote] = useState("");
+
     useEffect(() => {
         const verifyCookie = async () => {
         if (!cookies.token) {
@@ -35,19 +37,39 @@ const Home = () => {
     const Logout = () => {
         removeCookie("token");
         navigate("/signup");
+    
     };
-    return (
+    const getRandomQuotes = () => {
+        setQuote(quotes[Math.floor(Math.random() * quotes.length)]);
+      };
+    
+      useEffect(() => {
+        fetch('https://type.fit/api/quotes')
+          .then((resp) => resp.json())
+          .then((data) => {
+            setQuotes(data);
+            setQuote(data[Math.floor(Math.random() * data.length)]);
+          });
+      }, []);
+    
+      return (
         <>
-        <div className="home_page">
+          <div className="home_page">
             <h4>
-            {" "}
-            Welcome <span>{username}</span>
+              {" "}
+              Welcome <span>{username}</span>
             </h4>
             <button onClick={Logout}>LOGOUT</button>
-        </div>
-        <ToastContainer />
+          </div>
+          <div className="quote-container">
+            <h1>Random Quote</h1>
+            <blockquote>{quote.text}</blockquote>
+            <p>- {quote.author || 'Unknown'}</p>
+            <button onClick={getRandomQuotes}>Get Another Quote</button>
+          </div>
+          <ToastContainer />
         </>
-    );
-};
-
-export default Home;
+      );
+    };
+    
+    export default Home;
