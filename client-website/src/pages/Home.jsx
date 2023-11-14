92e1790d0acc3bb67663e6ca43a29e60c3982160
Ralfbrse <raphaeloburce@gmail.com>
2023-11-14 15:39:02 -0800

    Add .netlify folder to .gitignore and update API endpoint URLs

--- client-website/src/pages/Home.jsx
+++ client-website/src/pages/Home.jsx
@@ -9,50 +9,50 @@ import { ToastContainer, toast } from "react-toastify";
 */
 
 const Home = () => {
-    const navigate = useNavigate();
-    const [cookies, removeCookie] = useCookies([]);
-    const [username, setUsername] = useState("");
-    useEffect(() => {
-        const verifyCookie = async () => {
-        if (!cookies.token) {
-            navigate("/login");
-        }
-        const { data } = await axios.post(
-            "http://localhost:4000",
-            {},
-            { withCredentials: true }
-        );
-        const { status, user } = data;
-        setUsername(user);
-        return status
-            ? toast(`Hello ${user}`, {
-                position: "top-right",
-            })
-            : (removeCookie("token"), navigate("/login"));
-        };
-        verifyCookie();
-    }, [cookies, navigate, removeCookie]);
-    const Logout = () => {
-        removeCookie("token");
-        navigate("/signup");
+  const navigate = useNavigate();
+  const [cookies, removeCookie] = useCookies([]);
+  const [username, setUsername] = useState("");
+  useEffect(() => {
+    const verifyCookie = async () => {
+      if (!cookies.token) {
+        navigate("/login");
+      }
+      const { data } = await axios.post(
+        "https://anti-scatter-36f9c5f65c17.herokuapp.com/",
+        {},
+        { withCredentials: true }
+      );
+      const { status, user } = data;
+      setUsername(user);
+      return status
+        ? toast(`Hello ${user}`, {
+            position: "top-right",
+          })
+        : (removeCookie("token"), navigate("/login"));
     };
+    verifyCookie();
+  }, [cookies, navigate, removeCookie]);
+  const Logout = () => {
+    removeCookie("token");
+    navigate("/signup");
+  };
 
-    const Dashboard = () => {
-        navigate("/dashboard"); 
-    }; 
-    return (
-        <>
-        <div className="home_page">
-            <h4>
-            {" "}
-            Welcome <span>{username}</span>
-            </h4>
-            <button onClick={Logout}>LOGOUT</button>
-            <button onClick={Dashboard}>GO TO DASHBOARD</button>
-        </div>
-        <ToastContainer />
-        </>
-    );
+  const Dashboard = () => {
+    navigate("/dashboard");
+  };
+  return (
+    <>
+      <div className="home_page">
+        <h4>
+          {" "}
+          Welcome <span>{username}</span>
+        </h4>
+        <button onClick={Logout}>LOGOUT</button>
+        <button onClick={Dashboard}>GO TO DASHBOARD</button>
+      </div>
+      <ToastContainer />
+    </>
+  );
 };
 
 export default Home;
