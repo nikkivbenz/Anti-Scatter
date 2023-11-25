4bc706c1255ece88549cbbc3a36434e573ff0d4d
ralfbrse <raphaeloburce@gmail.com>
2023-11-24 19:30:20 -0800

    Refactor Header component

--- client-website/src/pages/Header.jsx
+++ client-website/src/pages/Header.jsx
@@ -1,33 +1,38 @@
-import React from 'react';
+import React from "react";
 import header_jumbotron_bg from "./header_jumbotron_bg.jpg";
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
 function Header() {
   return (
     <>
-    <div className="main-title h2" s>Anti-Scatter: Release your Productivity!</div>
-    <div className="jumbotron" style={{ backgroundImage: `url(${header_jumbotron_bg})` }}>
+      <div className="main-title h2" s>
+        Anti-Scatter: Release your Productivity!
+      </div>
+      <div
+        className="jumbotron"
+        style={{ backgroundImage: `url(${header_jumbotron_bg})` }}
+      >
+        <Container>
+          <div className="btn header-login-btn-wrapper">
+            <Button variant="warning" className="header-login-btn" href="/">
+              <div className="header-login-btn-text">Login</div>
+            </Button>{" "}
+          </div>
+          <p className="jumbotron-text">
+            Don't have an account yet? Please sign up here.{" "}
+          </p>
+        </Container>
 
-<Container>
-
-<div className="btn header-login-btn-wrapper">
-    <Button variant="warning" className="header-login-btn"><div className="header-login-btn-text">Login</div></Button>{' '}
-</div>
-<p className="jumbotron-text">
-       Don't have an account yet?  Please sign up here. </p>
-</Container>
-
-
-      {/* <p className="lead">
+        {/* <p className="lead">
         This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
       </p>
       <hr className="my-4" /> */}
-      {/* <p>
+        {/* <p>
         It uses utility classes for typography and spacing to space content out within the larger container.
       </p>
       <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a> */}
-    </div>
+      </div>
     </>
   );
 }
