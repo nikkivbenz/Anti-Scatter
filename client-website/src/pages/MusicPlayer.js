ab469d63968323e1c4d6a1c01eac3c498c6c5cba
Katherine Hernandez <katherinestacyh@gmail.com>
2023-12-07 11:49:23 -0800

    Adding the MusicPlayer file
    Currently in a temporary file , until we figure out where to put it

---
+++ client-website/src/pages/MusicPlayer.js
@@ -0,0 +1,36 @@
+//Temporary placement of Music Player
+import React, { useState } from 'react';
+
+const MusicPlayer = () => {
+    const [service, setService] = useState('youtube'); // default to YouTube
+
+    const renderPlayer = () => {
+        if (service === 'youtube') {
+            return (
+                <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/videoseries?list=YOUR_PLAYLIST_ID"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen>
+                </iframe>
+            );
+        } else if (service === 'spotify') {
+            // Spotify player integration goes here
+            // You will need to use Spotify's Web Playback SDK and handle authentication
+            return <div>Spotify player not implemented yet</div>;
+        }
+    };
+
+    return (
+        <div>
+            <button onClick={() => setService('youtube')}>Play YouTube Playlist</button>
+            <button onClick={() => setService('spotify')}>Play Spotify Playlist</button>
+            {renderPlayer()}
+        </div>
+    );
+};
+
+export default MusicPlayer;
