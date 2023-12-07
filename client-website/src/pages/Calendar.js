9c49aae06991dd11437620d8e81cc21747ee7d9a
Katherine Hernandez <katherinestacyh@gmail.com>
2023-12-06 19:40:19 -0800

    Adding in my GoogleCalendar Feature
    First attempt at using the Google Calendar API

--- client-website/src/pages/Calendar.js
+++ client-website/src/pages/Calendar.js
@@ -1,3 +1,5 @@
+{/*
+
 function Calendar() {
     return(
         <h1> i am in calendar </h1> 
@@ -5,3 +7,77 @@ function Calendar() {
 };
 
 export default Calendar; 
+
+*/}
+
+
+
+import React, { useState, useEffect } from 'react';
+import { gapi } from 'gapi-script';
+
+// Google API client ID and API key
+const CLIENT_ID = '111482971289803426523';
+const API_KEY = 'I need to find the API KEY to write in here';
+
+// Google Calendar API discovery document
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+
+// Which parts of the user's Google account I want to access
+const SCOPES = "https://www.googleapis.com/auth/calendar.events.readonly";
+
+const CalendarComponent = () => {
+  // Store the events from Google Calendar
+  const [events, setEvents] = useState([]);
+
+  // Effect hook to initialize the Google API client when the component mounts
+  useEffect(() => {
+    gapi.load('client:auth2', initClient);
+  }, []);
+
+  // Initializes the Google API client with my credentials and sets up sign-in state listeners
+  const initClient = () => {
+    gapi.client.init({
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      discoveryDocs: DISCOVERY_DOCS,
+      scope: SCOPES
+    }).then(() => {
+
+      // Listening for sign-in state changes
+      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+      // Handle the current sign-in state
+      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    }, error => {
+      console.error(error);
+    });
+  };
+
+  // Updates the sign-in status and fetches events if the user is signed in
+  const updateSigninStatus = (isSignedIn) => {
+    if (isSignedIn) {
+      listUpcomingEvents();
+    } else {
+      // Prompt the user to sign in
+      gapi.auth2.getAuthInstance().signIn();
+    }
+  };
+
+  // Fetches upcoming events from the user's calendar and updates the state
+  const listUpcomingEvents = () => {
+    gapi.client.calendar.events.list({
+      'calendarId': 'primary',
+      'timeMin': (new Date()).toISOString(),
+      'showDeleted': false,
+      'singleEvents': true,
+      'maxResults': 10,
+      'orderBy': 'startTime'
+    }).then(response => {
+      const events = response.result.items;
+      setEvents(events);
+    });
+  };
+
+};
+
+export default CalendarComponent;
