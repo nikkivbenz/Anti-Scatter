4fbadd11533fa4230f77b9f692bf3c8046484320
ralfbrse <raphaeloburce@gmail.com>
2023-12-08 00:00:41 -0800

    Refactor NavigationBar component

--- client-website/src/pages/NavigationBar.js
+++ client-website/src/pages/NavigationBar.js
@@ -1,93 +1,126 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import { Link } from "react-router-dom";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-import logo from './anti_scatter_logo.png'; 
+import logo from "./anti_scatter_logo.png";
 
 export default function NavigationBar() {
   return (
-    <>   
-    <div className="nav-container">
-      <Navbar className="bg-body-tertiary">
-        <Container>
-        <Nav.Link as={Link} to="/Dashboard" className="nav-link">Dashboard</Nav.Link>
-        </Container>
-      </Navbar>
-
-      <Navbar className="bg-body-tertiary">
-        <Container>
-        <Nav.Link as={Link} to="/StartSession" className="nav-link">Start A Session!</Nav.Link>
-        </Container>
-      </Navbar>
-
-      <Navbar className="bg-body-tertiary">
-        <Container>
-         <Nav.Link as={Link} to="/Blocklist" className="nav-link">Blocklist</Nav.Link>
-        </Container>
-      </Navbar>
-
-      <Navbar className="bg-body-tertiary">
-        <Container>
-        <Nav.Link as={Link} to="/Allowlist" className="nav-link">Allowlist</Nav.Link>
-        </Container>
-      </Navbar>
-
-      <Navbar className="bg-body-tertiary">
-        <Container>
-        <Nav.Link as={Link} to="/BlockByTheme" className="nav-link">Block By Theme</Nav.Link>
-        </Container>
-      </Navbar>
-
-      <Navbar className="bg-body-tertiary">
-        <Container>
-        <Nav.Link as={Link} to="/Blockschedule" className="nav-link">Block By Schedule</Nav.Link>
-        </Container>
-      </Navbar>
-
-      <Navbar className="bg-body-tertiary">
-        <Container>
-        <Nav.Link as={Link} to="/Calendar" className="nav-link">Calendar</Nav.Link>
-        </Container>
-      </Navbar>
-
-      <Navbar className="bg-body-tertiary">
-        <Container>
-        <Nav.Link as={Link} to="/Social" className="nav-link">Social</Nav.Link>
-        </Container>
-      </Navbar>
-
-
-      <Navbar className="bg-body-tertiary">
-        <Container>
-        <Nav.Link as={Link} to="/TodoList" className="nav-link">To-do List</Nav.Link>
-        </Container>
-      </Navbar>
-
-
-      <Navbar className="bg-body-tertiary">
-        <Container>
-        <Nav.Link as={Link} to="/Settings" className="nav-link">Setting</Nav.Link>
-        </Container>
-      </Navbar>
-
-
-      <Navbar className="bg-body-tertiary">
-        <Container>
-        <Nav.Link as={Link} to="/FAQ" className="nav-link">FAQ</Nav.Link>
-        </Container>
+    <>
+      <div className="nav-container">
+        <Navbar className="bg-body-tertiary">
+          <Container>
+            <Nav.Link as={Link} to="/Dashboard" className="nav-link">
+              Dashboard
+            </Nav.Link>
+          </Container>
         </Navbar>
 
-      <Navbar className="bg-body-tertiary" >
-        <Container>
-        <Nav.Link as={Link} to="/Feedback" className="nav-link">Give Us Feedback!</Nav.Link>
-        </Container>
-      </Navbar>
-      </div>  
-       {/* <Navbar className="navbar navbar-expand-lg  sidebar" id="sideNav" >
+        <Navbar className="bg-body-tertiary">
+          <Container>
+            <Nav.Link as={Link} to="/StartSession" className="nav-link">
+              Start A Session!
+            </Nav.Link>
+          </Container>
+        </Navbar>
+
+        <Navbar className="bg-body-tertiary">
+          <Container>
+            <Nav.Link as={Link} to="/Blocklist" className="nav-link">
+              Blocklist
+            </Nav.Link>
+          </Container>
+        </Navbar>
+
+        <Navbar className="bg-body-tertiary">
+          <Container>
+            <Nav.Link as={Link} to="/Allowlist" className="nav-link">
+              Allowlist
+            </Nav.Link>
+          </Container>
+        </Navbar>
+
+        <Navbar className="bg-body-tertiary">
+          <Container>
+            <Nav.Link as={Link} to="/BlockByTheme" className="nav-link">
+              Block By Theme
+            </Nav.Link>
+          </Container>
+        </Navbar>
+
+        <Navbar className="bg-body-tertiary">
+          <Container>
+            <Nav.Link as={Link} to="/Blockschedule" className="nav-link">
+              Block By Schedule
+            </Nav.Link>
+          </Container>
+        </Navbar>
+
+        <Navbar className="bg-body-tertiary">
+          <Container>
+            <Nav.Link as={Link} to="/Calendar" className="nav-link">
+              Calendar
+            </Nav.Link>
+          </Container>
+        </Navbar>
+
+        <Navbar className="bg-body-tertiary">
+          <Container>
+            <Nav.Link as={Link} to="/Social" className="nav-link">
+              Social
+            </Nav.Link>
+          </Container>
+        </Navbar>
+
+        <Navbar className="bg-body-tertiary">
+          <Container>
+            <Nav.Link as={Link} to="/TodoList" className="nav-link">
+              To-do List
+            </Nav.Link>
+          </Container>
+        </Navbar>
+
+        <Navbar className="bg-body-tertiary">
+          <Container>
+            <Nav.Link as={Link} to="/Settings" className="nav-link">
+              Setting
+            </Nav.Link>
+          </Container>
+        </Navbar>
+
+        <Navbar className="bg-body-tertiary">
+          <Container>
+            <Nav.Link as={Link} to="/FAQ" className="nav-link">
+              FAQ
+            </Nav.Link>
+          </Container>
+        </Navbar>
+
+        <Navbar className="bg-body-tertiary">
+          <Container>
+            <Nav.Link as={Link} to="/Feedback" className="nav-link">
+              Give Us Feedback!
+            </Nav.Link>
+          </Container>
+        </Navbar>
+
+        <Navbar className="bg-body-tertiary">
+          <Container>
+            <Nav.Link
+              as={Link}
+              to="https://chromewebstore.google.com/detail/quick-website-blocker/dlkgngbfjjcgblhbbpaicodbaihdkbdi?utm_source=app-launcher"
+              className="nav-link"
+            >
+              Extension
+            </Nav.Link>
+          </Container>
+        </Navbar>
+      </div>
+      {/* <Navbar className="navbar navbar-expand-lg  sidebar" id="sideNav" >
         <Container>
         <Nav className="me-auto">
 
@@ -105,7 +138,6 @@ export default function NavigationBar() {
           
         </Container>
       </Navbar> */}
-
-    </> 
+    </>
   );
 }
