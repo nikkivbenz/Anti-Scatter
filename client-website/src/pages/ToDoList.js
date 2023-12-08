876b2c4c9dbaa7e398706723385aa38f58ed7db4
ralfbrse <raphaeloburce@gmail.com>
2023-12-07 17:24:42 -0800

    Add ToDoList component with task management functionality

---
+++ client-website/src/pages/ToDoList.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [selectedTasks, setSelectedTasks] = useState([]);
+
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    setTasks(storedTasks);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    // Check for due tasks and show notifications
+    tasks.forEach((task) => {
+      const dueDateObj = new Date(task.dueDate);
+      const currentDate = new Date.now();
+
+      if (
+        dueDateObj.getFullYear() === currentDate.getFullYear() &&
+        dueDateObj.getMonth() === currentDate.getMonth() &&
+        dueDateObj.getDate() === currentDate.getDate()
+      ) {
+        // Notify user when the due date matches the current date
+        toast.info(`Task "${task.text}" is due today!`, { autoClose: 5000 });
+      }
+    });
+  }, [tasks]);
+
+  const addTask = () => {
+    if (newTask.trim() !== "" && dueDate !== "") {
+      setTasks([...tasks, { id: Date.now(), text: newTask, dueDate }]);
+      setNewTask("");
+      setDueDate("");
+    }
+  };
+
+  const toggleTaskSelection = (taskId) => {
+    const isSelected = selectedTasks.includes(taskId);
+    if (isSelected) {
+      setSelectedTasks(selectedTasks.filter((id) => id !== taskId));
+    } else {
+      setSelectedTasks([...selectedTasks, taskId]);
+    }
+  };
+
+  const removeSelectedTasks = () => {
+    const updatedTasks = tasks.filter(
+      (task) => !selectedTasks.includes(task.id)
+    );
+    setTasks(updatedTasks);
+    setSelectedTasks([]);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li
+            key={task.id}
+            onClick={() => toggleTaskSelection(task.id)}
+            style={{
+              textDecoration: selectedTasks.includes(task.id)
+                ? "line-through"
+                : "none",
+            }}
+          >
+            {task.text} - Due Date: {task.dueDate}
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          placeholder="Task"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <input
+          type="date"
+          value={dueDate}
+          onChange={(e) => setDueDate(e.target.value)}
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      {selectedTasks.length > 0 && (
+        <div>
+          <button onClick={removeSelectedTasks}>Remove Selected</button>
+        </div>
+      )}
+
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default TodoList;
